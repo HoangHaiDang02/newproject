8ae623a1d3e92503e4736df980d8d03038549adf
Hai Dang <hoanghaidang115@gmail.com>
2022-05-04 09:37:26 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// function forx() {
+//     var myString='';
+//     for(var aram of arguments)
+//     {
+//         myString+=`${aram} -`;
+//     }
+//     console.log(myString);
+// }
+// forx("hoang hai dang ",12,2,3,4,5);
+
+function  cong(a,b)
+{
+    return a+b;
+}
+var result = cong(5,6);
+
+console.log(typeof result);
